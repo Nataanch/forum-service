4700cba52ceeda9821ca2b705d48a6e836d142f2
Nataanch <scandyyroom@gmail.com>
2023-09-26 09:20:52 +0300

    Log

--- src/main/java/telran/java48/ForumServiceApplication.java
+++ src/main/java/telran/java48/ForumServiceApplication.java
@@ -3,11 +3,16 @@ package telran.java48;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+
 @SpringBootApplication
 public class ForumServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumServiceApplication.class, args);
+		
+		System.out.println("nnn");
 	}
+	
 
 }
